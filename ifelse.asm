93b1041c852853cc5a330458986b27b8c775a4df
JessicaAzcona <38532189+JessicaAzcona@users.noreply.github.com>
2022-03-15 20:05:27 -0600

    Update ifelse.asm

--- ifelse.asm
+++ ifelse.asm
@@ -24,7 +24,9 @@ move $t1, $sv0		#ahora Y va a t1
 li $t3,1		#arregla para que t3 = 1
 
 
-beq $t0, $t1, ELSE		#if x == y then ELSE
+beq $t0, #t3 ELSE		#if x == 1 then ELSE
+add $t1, $t0, $t0		# y - x + x
+li $t0, 0			#x - 0
 li $st, 4			#imrpime el nuevo string
 la $a0, final			#elige "Final result"
 syscall
@@ -41,8 +43,7 @@ move $a0, $t0			#Mueve x a la consola
 li $v0, 1				#imprime la consola
 syscall
 j Exit						#sale del loop
-ELSE: 
-add $t1, $t2, $t3		#esle
+ELSE: add $t1, $t2, $t3		#esle, y += 1
 li $v0, 4			#imrprime string
 la $a0, final			#elige "Final Result"
 syscall
